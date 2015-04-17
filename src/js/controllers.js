fc564e3ca1999709a4f1007e846bde5786724f6c
icastilho <italorfc@gmail.com>
2015-04-17 20:52:13 -0300

    index new menu

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -6,40 +6,13 @@ var controller = angular.module('carlosvilela.controller', []);
 
 controller.controller('MenuCtrl', function ($scope) {
 
-    $scope.slidemenu = function (){
-        console.log(1);
-        //e.preventDefault();
-        var distance = $('article').css('left');
-        if(distance == "auto" || distance == "0px") {
-            openSidepage();
-        }else{
-            closeSidepage();
-        }
-    }
-
-    function openSidepage() {
-        $('article').animate({
-                left: '300px'
-            },
-            450, 'easeOutBack').promise().done(function (){
-                $('nav').css('z-index', '1');
-            });
-    }
-
-    function closeSidepage(){
-        $('nav').css('z-index', '0');
-        $('article').animate({
-            left: '0px'
-        }, 500, 'easeOutQuint');
-    }
-
 
 
 })
 .controller('PortifolioCtrl', function ($scope) {
     $scope.phones = "Home Carlos Vilela";
     $(".diamondswrap").diamonds({
-        size : 450,
+        size : 440,
         gap : 1,
         hideIncompleteRow :false,
         autoRedraw :true,
